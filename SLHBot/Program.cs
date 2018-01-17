4c9a8f4a9d29dccafe743517a92b52365072e662
Tarocco <bun@taroc.co>
2018-01-16 21:47:47 -0800

    Move standalone logic to its own file

--- SLHBot/Program.cs
+++ SLHBot/Program.cs
@@ -114,45 +114,10 @@ namespace SLHBot
             if (!IsNullOrEmpty(standalone_binding_address))
             {
                 // KISS HTTP server
-                var listener = new HttpListener();
                 var prefix = $"http://{standalone_binding_address}/";
+                var listener = new HttpListener();
                 listener.Prefixes.Add(prefix);
-                Task.Run(async () =>
-                {
-                    listener.Start();
-                    for (; ; )
-                    {
-                        var context = await listener.GetContextAsync();
-                        string response;
-                        HttpStatusCode http_status_code;
-                        try
-                        {
-                            var request_path = context.Request.RawUrl.TrimStart('/');
-                            var root = new Uri(Directory.GetCurrentDirectory() + "/html/");
-                            var file_path_uri = new Uri(root, request_path);
-                            if (!root.IsBaseOf(file_path_uri))
-                                throw new Exception("Bad file path URI");
-                            if (file_path_uri.AbsolutePath.EndsWith("/"))
-                                file_path_uri = new Uri(file_path_uri, "index.html");
-                            using (var reader = new StreamReader(file_path_uri.AbsolutePath))
-                                response = reader.ReadToEnd();
-                            http_status_code = HttpStatusCode.OK;
-                        }
-                        catch (Exception ex)
-                        {
-                            response = $"{ex.Message}\r\n{ex.StackTrace}";
-                            http_status_code = HttpStatusCode.InternalServerError;
-                        }
-
-                        context.Response.StatusCode = (int)http_status_code;
-                        using (var writer = new StreamWriter(context.Response.OutputStream))
-                        {
-                            await writer.WriteAsync(response);
-                            await writer.FlushAsync();
-                        }
-                    }
-                });
-
+                Standalone.StartListener(listener);
                 Console.WriteLine($"Running standalone http server on {prefix}");
             }
 
---
+++ SLHBot/Standalone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLHBot
+{
+    static class Standalone
+    {
+        public static void StartListener(HttpListener listener)
+        {
+            Task.Run(async () =>
+            {
+                listener.Start();
+                for (; ; )
+                {
+                    var context = await listener.GetContextAsync();
+                    string response;
+                    HttpStatusCode http_status_code;
+                    try
+                    {
+                        var request_path = context.Request.RawUrl.TrimStart('/');
+                        var root = new Uri(Directory.GetCurrentDirectory() + "/html/");
+                        var file_path_uri = new Uri(root, request_path);
+                        if (!root.IsBaseOf(file_path_uri))
+                            throw new Exception("Bad file path URI");
+                        if (file_path_uri.AbsolutePath.EndsWith("/"))
+                            file_path_uri = new Uri(file_path_uri, "index.html");
+                        using (var reader = new StreamReader(file_path_uri.AbsolutePath))
+                            response = reader.ReadToEnd();
+                        http_status_code = HttpStatusCode.OK;
+                    }
+                    catch (Exception ex)
+                    {
+                        response = $"{ex.Message}\r\n{ex.StackTrace}";
+                        http_status_code = HttpStatusCode.InternalServerError;
+                    }
+
+                    context.Response.StatusCode = (int)http_status_code;
+                    using (var writer = new StreamWriter(context.Response.OutputStream))
+                    {
+                        await writer.WriteAsync(response);
+                        await writer.FlushAsync();
+                    }
+                }
+            });
+        }
+    }
+}
