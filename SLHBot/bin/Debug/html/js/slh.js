546944b2ca520b7b5957c4cc4133a6f7f07887e9
Tarocco <bun@taroc.co>
2018-07-06 02:48:32 -0700

    Complete restructure needs docs lol

--- SLHBot/bin/Debug/html/js/slh.js
+++ SLHBot/bin/Debug/html/js/slh.js
@@ -1,3 +1,5 @@
+const NULL_KEY = "00000000-0000-0000-0000-000000000000";
+
 function uuid() {
     var uuid = "", i, random;
     for (i = 0; i < 32; i++) {
@@ -9,95 +11,101 @@ function uuid() {
     return uuid;
 }
 
-class Standalone {
-    //get Socket() { return this._Socket; }
-    //get Remote() { return this._Remote; }
-    get Client() { return this._Client; }
-
+class StandaloneProperties {
     constructor() {
-        var remote = new JRPC({ client: true });
-        this._Remote = remote;
-
-        var client = {
-            Eval: function (expression, ...args) {
-                var promise = new Promise(function (resolve, reject) {
-                    remote.call("Client/Eval", [expression, args], function (error, result) { resolve(result); });
-                });
-                return promise;
-            },
-            EvalSet: function (expression, value) {
-                var promise = new Promise(function (resolve, reject) {
-                    remote.call("Client/Eval/Set", [expression, [value]], function (error, result) { resolve(result); });
-                });
-                return promise;
-            },
-            EvalAddEventHandler: function (expression, handler) {
-                var handler_id = uuid();
-                var promise = new Promise(function (resolve, reject) {
-                    remote.call("Client/Eval/AddEventHandler", [expression, handler_id], function (error, result) { resolve(handler_id); });
-                    var entry = {};
-                    entry[handler_id] = function (params, next) {
-                        try {
-                            handler(...params);
-                            next(false);
-                        }
-                        catch{
-                            next(true);
-                        }
-                    };
-                    remote.expose(entry);
-                    remote.upgrade();
-                });
-                return promise;
-            }
-        };
-        this._Client = client;
+        this._Sockets = {};
     }
-
-    async Connect(ws_address) {
-        var _this = this;
-        var promise = new Promise(function (resolve, reject) {
-            var socket = new WebSocket(ws_address, "SLH-Message-Protocol-0001");
-            _this._Socket = socket;
-
-            var previous_onopen = socket.onopen;
-            socket.onopen = function (event) {
-                resolve(true);
-                socket.onopen = previous_onopen;
-                if (typeof previous_onopen === "function")
-                    previous_onopen(event);
+    async CloseNamedSocket(name) {
+        return new Promise(async (resolve, reject) => {
+            let socket = this._Sockets[name];
+            if (socket instanceof WebSocket) {
+                socket.onerror = (e) => {
+                    socket.onerror = null;
+                    socket.onopen = null;
+                    reject(socket);
+                };
+                socket.onclose = (e) => {
+                    socket.onerror = null;
+                    socket.onopen = null;
+                    resolve(socket);
+                };
+                socket.close();
+            }
+            else {
+                resolve();
+            }
+        });
+    }
+    async ReopenNamedSocket(name, address) {
+        if (typeof address === typeof undefined)
+            address = name;
+        return new Promise(async (resolve, reject) => {
+            await this.CloseNamedSocket(name);
+            let socket = new WebSocket(address);
+            this._Sockets[name] = socket;
+            socket.onerror = (e) => {
+                socket.onerror = null;
+                socket.onopen = null;
+                reject(socket);
             };
-
-            var remote = _this._Remote;
-
-            socket.onmessage = function (event) {
-                remote.receive(event.data);
+            socket.onopen = (e) => {
+                socket.onerror = null;
+                socket.onopen = null;
+                resolve(socket);
             };
+        });
+    }
+}
 
-            remote.setTransmitter(function (message, next) {
-                try {
-                    socket.send(message);
-                    return next(false);
-                } catch (e) {
-                    return next(true);
-                }
-            });
+let Standalone = new StandaloneProperties();
+
+class SLHClient {
+    constructor(socket) {
+        let remote = new JRPC({ client: true });
+        this._Remote = remote;
+        this._Socket = socket;
+
+        socket.onmessage = function (event) {
+            remote.receive(event.data);
+        };
+
+        remote.setTransmitter(function (message, next) {
+            try {
+                socket.send(message);
+                return next(false);
+            } catch (e) {
+                return next(true);
+            }
+        });
+    }
+    Eval (expression, ...args) {
+        let promise = new Promise((resolve, reject) => {
+            this._Remote.call("Client/Eval", [expression, args], function (error, result) { resolve(result); });
         });
         return promise;
     }
-
-    Close() {
-        var remote = this._Remote;
-        var socket = this._Socket;
-        var promise = new Promise(function (resolve, reject) {
-            var previous_onclose = socket.onclose;
-            socket.onclose = function (event) {
-                resolve(true);
-                socket.onclose = previous_onclose;
-                if (typeof previous_onclose === "function")
-                    previous_onclose(event);
+    EvalSet (expression, value) {
+        let promise = new Promise((resolve, reject) => {
+            this._Remote.call("Client/Eval/Set", [expression, [value]], function (error, result) { resolve(result); });
+        });
+        return promise;
+    }
+    EvalAddEventHandler (expression, handler) {
+        let handler_id = uuid();
+        var promise = new Promise((resolve, reject) => {
+            this._Remote.call("Client/Eval/AddEventHandler", [expression, handler_id], function (error, result) { resolve(handler_id); });
+            let entry = {};
+            entry[handler_id] = (params, next) => {
+                try {
+                    handler(...params);
+                    next(false);
+                }
+                catch{
+                    next(true);
+                }
             };
-            socket.close();
+            this._Remote.expose(entry);
+            this._Remote.upgrade();
         });
         return promise;
     }
