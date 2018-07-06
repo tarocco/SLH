b90e9f45b66cb52237a8eff64f357705f5831b05
Tarocco <bun@taroc.co>
2018-07-06 02:47:22 -0700

    simulators to handles, don't convert avatars

--- LibSLH/SLHConverter.cs
+++ LibSLH/SLHConverter.cs
@@ -4,35 +4,22 @@ using System;
 
 namespace LibSLH
 {
-    /// <summary>
-    /// To address issues with automatic Int64 deserialization -- see https://stackoverflow.com/a/9444519/1037948
-    /// </summary>
     public class SLHConverter : JsonConverter
     {
-        #region Overrides of JsonConverter
-
-        /// <summary>
-        /// Only want to deserialize
-        /// </summary>
         public override bool CanWrite { get { return true; } }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            // Unused if CanWrite == false
-            writer.WriteValue(value.ToString());
+            if (value is Simulator)
+                writer.WriteValue(((Simulator)value).Handle);
+            else
+                writer.WriteValue(value.ToString());
         }
 
-        /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader"/> to read from.</param><param name="objectType">Type of the object.</param><param name="existingValue">The existing value of object being read.</param><param name="serializer">The calling serializer.</param>
-        /// <returns>
-        /// The object value.
-        /// </returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (reader.TokenType == JsonToken.Integer)
-                return Convert.ToInt32(reader.Value);     // convert to Int32 instead of Int64
+                return Convert.ToInt32(reader.Value); // convert to Int32 instead of Int64
             if (reader.TokenType == JsonToken.String)
             {
                 {
@@ -44,19 +31,14 @@ namespace LibSLH
                         return result;
                 }
             }
-            return serializer.Deserialize(reader);   // default to regular deserialization
+            return serializer.Deserialize(reader);
         }
 
-        /// <summary>
-        /// Determines whether this instance can convert the specified object type.
-        /// </summary>
-        /// <param name="objectType">Type of the object.</param>
-        /// <returns>
-        /// <c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
-        /// </returns>
         public override bool CanConvert(Type objectType)
         {
             var non_converting =
+                // Custom types
+                objectType == typeof(Avatar) ||
                 objectType.IsSubclassOf(typeof(GridClient)) ||
                 objectType == typeof(AgentManager);
 
@@ -70,13 +52,11 @@ namespace LibSLH
                 objectType == typeof(UUID) ||
                 objectType == typeof(Vector3) ||
                 objectType.IsSubclassOf(typeof(Primitive)) ||
-                // need this last one in case we "weren't given" the type
-                // and this will be accounted for by `ReadJson` checking tokentype
+                objectType == typeof(Simulator) ||
+                // Default
                 objectType == typeof(object);
 
             return !non_converting && converting;
         }
-
-        #endregion Overrides of JsonConverter
     }
 }
